f1576012b1852b3d9292d315216a88b605e5bc31
Jadin Clayton <103282590+whoisjadin@users.noreply.github.com>
2024-12-19 01:26:41 -0600

    new file:   .github/workflows/firebase-hosting-merge.yml 	new file:   .github/workflows/firebase-hosting-pull-request.yml 	modified:   sketcha/.gitignore 	modified:   sketcha/package-lock.json 	modified:   sketcha/package.json 	modified:   sketcha/vite.config.js

--- sketcha/vite.config.js
+++ sketcha/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+    index: 'src/index.html', // Update this line
+  },
   plugins: [react()],
 })
