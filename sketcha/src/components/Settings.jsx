c77c39002f3194f74cfe0f40a493673dacfcaf2d
Jadin Clayton <103282590+whoisjadin@users.noreply.github.com>
2024-12-22 01:55:57 -0600

    modified:   sketcha/index.html 	modified:   sketcha/src/components/Settings.jsx 	modified:   sketcha/src/index.css

--- sketcha/src/components/Settings.jsx
+++ sketcha/src/components/Settings.jsx
@@ -1,21 +1,70 @@
+import { useState } from "react";
+
 function Settings({ stageRef, canvasSize, setCanvasSize, setIsSketchLoaded, setFromSettings }) {
+    const [fileName, setFileName] = useState('My Sketch');
+    const [previewImage, setPreviewImage] = useState(null);
+
     const handleWidthChange = (e) => setCanvasSize({ ...canvasSize, width: parseInt(e.target.value) });
     const handleHeightChange = (e) => setCanvasSize({ ...canvasSize, height: parseInt(e.target.value) });
 
     const handleSave = () => {
-        if (stageRef.current) {
-            const width = stageRef.current.width();
-            const height = stageRef.current.height();
-            const dataURL = stageRef.current.toDataURL({
-                width,
-                height,
-                mimeType: 'image/png',
-            });
-            const link = document.createElement('a');
-            link.href = dataURL;
-            link.download = 'image.png';
-            link.click();
-        }
+        if (!stageRef.current) return;
+
+        const width = stageRef.current.width();
+        const height = stageRef.current.height();
+        const dataURL = stageRef.current.toDataURL({
+            width,
+            height,
+            mimeType: 'image/png',
+        });
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataURL;
+        downloadLink.download = fileName;
+        downloadLink.click();
+    };
+
+    const handleCopy = () => {
+        if (!stageRef.current) return;
+
+        const width = stageRef.current.width();
+        const height = stageRef.current.height();
+        const dataURL = stageRef.current.toDataURL({
+            width,
+            height,
+            mimeType: 'image/png',
+        });
+
+        const image = new Image();
+        image.src = dataURL;
+        image.onload = () => {
+            const canvasElement = document.createElement('canvas');
+            canvasElement.width = image.width;
+            canvasElement.height = image.height;
+            const context = canvasElement.getContext('2d');
+            context.drawImage(image, 0, 0);
+            canvasElement.toBlob((blob) => {
+                navigator.clipboard.write([
+                    new ClipboardItem({
+                        'image/png': blob,
+                    }),
+                ]);
+            }, 'image/png');
+        };
+    };
+
+    const handlePreview = () => {
+        if (!stageRef.current) return;
+
+        const width = stageRef.current.width();
+        const height = stageRef.current.height();
+        const dataURL = stageRef.current.toDataURL({
+            width,
+            height,
+            mimeType: 'image/png',
+        });
+
+        setPreviewImage(dataURL);
     };
 
     return (
@@ -23,10 +72,9 @@ function Settings({ stageRef, canvasSize, setCanvasSize, setIsSketchLoaded, setF
             <p className="text-xs font-bold tracking-wide">Settings</p>
             <div className="flex flex-rows space-x-4">
                 <div className=" flex-cols w-48 space-y-2">
-                    <button className="btn btn-sm border rounded-md w-full border-primary-content" onClick={handleSave}>Save Sketch as PNG</button>
-                    <button className="btn btn-sm btn-disabled rounded-md w-full" disabled>Copy Sketch to Clipboard</button>
-                    <button className="btn btn-sm border rounded-md w-full border-primary-content" onClick={() => document.getElementById('new_sketch_warning').showModal()}>New Sketch</button>
-
+                    <button className="btn btn-sm border rounded-md w-full border-primary-content" onClick={() => { handlePreview(); document.getElementById('save_sketch_preview').showModal(); }}>Save Sketch</button>
+                    <button className="btn btn-sm border rounded-md w-full border-primary-content" onClick={handleCopy}>Copy Sketch to Clipboard</button>
+                    <button className="btn btn-sm border rounded-md w-full border-primary-content" onClick={() => { document.getElementById('new_sketch_warning').showModal(); }}>New Sketch</button>
                 </div>
                 <div className=" flex-cols w-32">
                     <label className="text-xs font-thin tracking-wide">Width:</label>
@@ -42,7 +90,29 @@ function Settings({ stageRef, canvasSize, setCanvasSize, setIsSketchLoaded, setF
                     <div className="modal-action flex-col space-x-0 space-y-2">
                         <button className="btn border bg-base-100 rounded-md w-full border-primary-content" onClick={() => { setIsSketchLoaded(false); setFromSettings(true); }}>Create New Sketch</button>
                         <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn border rounded-md w-full border-primary-content">Cancel</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+            <dialog id="save_sketch_preview" className="modal">
+                <div className="modal-box rounded-md">
+                    <h3 className="font-bold text-lg mb-4">Save Sketch</h3>
+                    <div className="flex flex-row space-x-6">
+                        <div className="flex flex-col w-full">
+                        <p className="text-sm">Sketch Name:</p>
+                        <input type="text" value={fileName} className="input input-bordered border-primary-content rounded-md input-sm px-1 w-full" placeholder="Sketch name" onChange={(e) => setFileName(e.target.value)} />
+
+                    <button className="mt-4 btn border bg-base-100 rounded-md border-primary-content w-full" onClick={handleSave}>Save as PNG</button>
+                    </div>
+                    <div className="flex-none flex w-40 sm:w-64 h-40 sm:h-64 justify-center items-center border border-primary-content rounded-sm p-2 bg-base-200">
+                        {previewImage && (
+                            <img src={previewImage} alt="Preview" className="bg-base-100 sm:w-52 w-20 sm:h-52 w-20 m-auto shadow-sm" />
+                        )}
+                    </div>
+                    </div>
+                    <div className="modal-action flex-col space-x-0 space-y-2">
+                        <form method="dialog">
                             <button className="btn border rounded-md w-full border-primary-content">Cancel</button>
                         </form>
                     </div>
@@ -52,3 +122,4 @@ function Settings({ stageRef, canvasSize, setCanvasSize, setIsSketchLoaded, setF
     );
 }
 export default Settings;
+
